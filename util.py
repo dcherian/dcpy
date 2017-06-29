7dc4527fd0db4e1fbc2be1536b7f3f389a4583d6
deepak <dpak.cherian@gmail.com>
2017-06-29 15:20:26 -0700

    Whitespace fixes.

--- util.py
+++ util.py
@@ -15,18 +15,22 @@ def datenum2datetime(matlab_datenum):
 
     return np.asarray(python_datetime)
 
+
 def smooth(x, window_len=11, window='hanning'):
     """smooth the data using a window with requested size.
 
-    This method is based on the convolution of a scaled window with the signal.
+    This method is based on the convolution of a scaled window
+    with the signal.
     The signal is prepared by introducing reflected copies of the signal
-    (with the window size) in both ends so that # TODO: ransient parts are minimized
-    in the begining and end part of the output signal.
+    (with the window size) in both ends so that transient parts are
+    minimized in the begining and end part of the output signal.
 
     input:
         x: the input signal
-        window_len: the dimension of the smoothing window; should be an odd integer
-        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+        window_len: the dimension of the smoothing window;
+                    should be an odd integer
+        window: the type of window from 'flat', 'hanning',
+                'hamming', 'bartlett', 'blackman'
         flat window will produce a moving average smoothing.
 
     output:
@@ -38,13 +42,11 @@ def smooth(x, window_len=11, window='hanning'):
     x=sin(t)+randn(len(t))*0.1
     y=smooth(x)
 
-     see also:
-
-    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
-    scipy.signal.lfilter
-
-    TODO: the window parameter could be the window itself if an array instead of a string
-    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    TODO: the window parameter could be the window itself if an
+          array instead of a string
+    NOTE: length(output) != length(input), to correct this:
+          return y[(window_len/2-1):-(window_len/2)]
+          instead of just y.
     """
 
     import numpy as np
