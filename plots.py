640cd2e62b4228d90838fc6be67c2662c183016d
deepak <dpak.cherian@gmail.com>
2017-06-30 17:04:26 -0700

    normalized histograms by default.

--- plots.py
+++ plots.py
@@ -6,10 +6,12 @@ def hist(var, log=False, bins=100, alpha=0.5, normed=True, **kwargs):
     if log:
         var = np.log10(abs(var))
 
-    plt.hist(var[np.isfinite(var)], bins=bins, alpha=alpha, normed=normed, **kwargs)
+    plt.hist(var[np.isfinite(var)], bins=bins, alpha=alpha,
+             normed=normed, **kwargs)
 
 
 def line45():
+
     import matplotlib.pyplot as plt
     import numpy as np
 
