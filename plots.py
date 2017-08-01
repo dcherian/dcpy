976dd335a06384d584028fc677ac0cef2020bd34
deepak <dpak.cherian@gmail.com>
2017-08-01 14:43:26 -0600

    Add linex, liney

--- plots.py
+++ plots.py
@@ -2,27 +2,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def linex(var, color='gray', linestyle='--', zorder=-1):
+def linex(var, ax=None, color='gray', linestyle='--', zorder=-1):
+
+    if ax is None:
+        ax = plt.gca()
 
     var = np.array(var, ndmin=1)
     for vv in var:
-        plt.axvline(vv, color=color, linestyle=linestyle, zorder=zorder)
+        ax.axvline(vv, color=color, linestyle=linestyle, zorder=zorder)
 
 
-def liney(var, color='gray', linestyle='--', zorder=-1):
+def liney(var, ax=None, color='gray', linestyle='--', zorder=-1):
+
+    if ax is None:
+        ax = plt.gca()
 
     var = np.array(var, ndmin=1)
     for vv in var:
-        plt.axhline(vv, color=color, linestyle=linestyle, zorder=zorder)
+        ax.axhline(vv, color=color, linestyle=linestyle, zorder=zorder)
 
 
-def hist(var, log=False, bins=100, alpha=0.5, normed=True, **kwargs):
+def hist(var, log=False, bins=100, alpha=0.5, normed=True,
+         mark95=False, **kwargs):
 
+    var = var.copy()
     if log:
         var = np.log10(abs(var))
 
-    plt.hist(var[np.isfinite(var)], bins=bins, alpha=alpha,
+    var = var[np.isfinite(var)]
+    plt.hist(var, bins=bins, alpha=alpha,
              normed=normed, **kwargs)
+
+    if mark95 is True:
+        from dcpy.util import calc95
+        linex(calc95(var))
 
 
 def line45():
