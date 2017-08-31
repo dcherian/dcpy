c2d3dc655f07925595836399d3382842ec21b772
deepak <dpak.cherian@gmail.com>
2017-08-31 15:34:15 -0700

    Iterations progress.

--- util.py
+++ util.py
@@ -170,3 +170,33 @@ def BinEqualizeHist(coords, bins=10, offset=0.0, label=None):
                           offset+(1.0-offset)*transformed/span,
                           np.nan)
     return xs[:-1], ys[:-1], np.ma.masked_invalid(compressed)
+
+
+# Print iterations progress
+def print_progress(iteration, total, prefix='', suffix='',
+                   decimals=1, bar_length=100):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals
+                                  in percent complete (Int)
+        bar_length  - Optional  : character length of bar (Int)
+    """
+
+    import sys
+    str_format = "{0:." + str(decimals) + "f}"
+    percents = str_format.format(100 * (iteration / float(total)))
+    filled_length = int(round(bar_length * iteration / float(total)))
+    bar = '█' * filled_length + '-' * (bar_length - filled_length)
+
+    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar,
+                                            percents, '%', suffix)),
+
+    if iteration == total:
+        sys.stdout.write('\n')
+
+    sys.stdout.flush()
