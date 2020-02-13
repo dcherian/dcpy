4b2711a06bc7155c89975a1aa3d6c8f14353bc77
dcherian <deepak@cherian.net>
2020-02-13 16:40:23 -0700

    Some useful printing in batch_to_zarr

--- dcpy/dask.py
+++ dcpy/dask.py
@@ -133,9 +133,10 @@ def batch_to_zarr(ds, file, dim, batch_size, restart=False, **kwargs):
     if not restart:
         ds.isel({dim: [0]}).to_zarr(file, consolidated=True, mode="w", **kwargs)
     else:
+        print("Restarting...")
         opened = xr.open_zarr(file, consolidated=True)
         ds = ds.sel(time=slice(opened[dim][-1], None))
-        print(f"Restarting. Last index = {opened[dim][-1].values}. Starting from {ds[dim][1].values}")
+        print(f"Last index = {opened[dim][-1].values}. Starting from {ds[dim][1].values}")
         opened.close()
 
     for t in tqdm.tqdm(range(1, ds.sizes[dim], batch_size)):
