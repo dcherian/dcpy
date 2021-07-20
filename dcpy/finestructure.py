bffc37554f28ae26a2c6dcdfa34f213240f566cd
dcherian <deepak@cherian.net>
2021-07-20 12:06:32 -0600

    Move finestructure code over

--- dcpy/finestructure.py
+++ dcpy/finestructure.py
@@ -1,5 +1,6 @@
 import functools
 
+import cf_xarray as cfxr
 import gsw
 import matplotlib.pyplot as plt
 import mixsea
@@ -7,6 +8,139 @@ import numpy as np
 from scipy import signal
 
 import xarray as xr
+
+from . import eos, oceans, plots
+
+
+def trim_mld_mode_water(profile):
+    """
+    Follows Whalen's approach of using a threshold criterion first to identify MLD,
+    trimming that; and then applying again to find mode water. I apply both T, σ criteria
+    and pick the max depth.
+    """
+
+    def find_thresh_delta(delta, thresh):
+        """Finds threshold in delta."""
+        return delta.cf["Z"].cf.isel(Z=delta > thresh)[0].data
+
+    T = profile.cf.standard_names["sea_water_temperature"][0]
+    σ = profile.cf.standard_names["sea_water_potential_density"][0]
+
+    assert (profile.cf["Z"] > 0).all().data
+    near_surf = profile.cf[[T, σ]].cf.sel(Z=10, method="nearest")
+    delta = np.abs(profile - near_surf)
+
+    Tmld = find_thresh_delta(delta[T], 0.2)
+    σmld = find_thresh_delta(delta[σ], 0.03)
+
+    trimmed = profile.cf.sel(Z=slice(max(Tmld, σmld), None))
+
+    near_surf = trimmed.cf[[T, σ]].cf.isel(Z=0)
+    delta = np.abs(trimmed - near_surf)
+    Tmode = find_thresh_delta(delta[T], 0.2)
+    σmode = find_thresh_delta(delta[σ], 0.03)
+
+    return profile.cf.sel(Z=slice(max(Tmode, σmode), None)).assign_coords(
+        Tmode=Tmode, σmode=σmode, Tmld=Tmld, σmld=σmld
+    )
+
+
+def results_to_xarray(results, profile):
+    data_vars = {
+        var: ("pressure", results[var])
+        for var in [
+            "ε",
+            "Kρ",
+            "N2mean",
+            "ξvar",
+            "ξvargm",
+            "Tzlin",
+            "Tzmean",
+            "mean_dTdz_seg",
+        ]
+    }
+    coords = {
+        var: ("pressure", results[var]) for var in ["flag", "pressure", "npts", "γmean"]
+    }
+    coords.update(
+        {
+            "latitude": profile.cf["latitude"].data,
+            "longitude": profile.cf["longitude"].data,
+            "γ_bounds": (("pressure", "nbnds"), results["γbnds"]),
+            "p_bounds": (("pressure", "nbnds"), results["pbnds"]),
+        }
+    )
+
+    turb = xr.Dataset(data_vars, coords)
+
+    turb.ε.attrs = {"long_name": "$ε$", "units": "W/kg"}
+    turb.Kρ.attrs = {"long_name": "$K_ρ$", "units": "m²/s"}
+    turb.ξvar.attrs = {"long_name": "$ξ$", "description": "strain variance"}
+    turb.ξvargm.attrs = {"long_name": "$ξ_{gm}$", "description": "GM strain variance"}
+    turb.N2mean.attrs = {
+        "long_name": "$N²$",
+        "description": "mean of quadratic fit of N² with pressure",
+    }
+    turb.Tzlin.attrs = {
+        "long_name": "$T_z^{lin}$",
+        "description": "linear fit of T vs pressure",
+    }
+    turb.Tzmean.attrs = {
+        "long_name": "$T_z^{quad}$",
+        "description": "mean of quadratic fit of Tz with pressure; like N² fitting for strain",
+    }
+    turb.mean_dTdz_seg.attrs = {
+        "description": "mean of dTdz values in segment",
+        "long_name": "$⟨T_z⟩$",
+    }
+    turb.pressure.attrs = {
+        "axis": "Z",
+        "standard_name": "sea_water_pressure",
+        "positive": "down",
+        "bounds": "p_bounds",
+    }
+    turb.npts.attrs = {"description": "number of points in segment"}
+    turb.γmean.attrs = {"bounds": "γ_bounds"}
+    turb.γmean.attrs.update(profile.γ.attrs)
+
+    turb.cf.guess_coord_axis()
+
+    for var in ["Tmld", "σmld", "Tmode", "σmode"]:
+        turb[var] = profile[var].data
+        turb[var].attrs["units"] = "m"
+        if "mld" in var:
+            turb[var].attrs["description"] = f"Δ{var[0]} criterion applied first time"
+        if "mode" in var:
+            turb[var].attrs["description"] = f"Δ{var[0]} criterion applied second time"
+
+    turb.flag.attrs = {
+        "flag_values": [-2, -1, 1, 2, 3, 4, 5],
+        "flag_meanings": "too_coarse too_short N²_variance_too_high too_unstratified too_little_bandwidth no_internal_waves good_data",
+    }
+
+    turb["χ"] = 2 * turb.Kρ * turb.Tzmean ** 2
+    turb.χ.attrs = {"long_name": "$χ$", "units": "°C²/s"}
+    turb["KtTz"] = turb.Kρ * turb.Tzmean
+    turb.KtTz.attrs = {"long_name": "$K_ρθ_z$", "units": "°Cm/s"}
+
+    return turb
+
+
+def choose_bins(pres, dz_segment):
+    lefts = np.sort(
+        np.hstack(
+            [
+                np.arange(1000 - dz_segment // 2, pres[0], -dz_segment // 2),
+                np.arange(1000, pres[-1] + 1, dz_segment // 2),
+            ]
+        )
+    )
+    rights = lefts + dz_segment
+
+    # lefts = np.arange(pres[0], pres[-1] + 1, dz_segment // 2)
+    # rights = lefts + dz_segment
+
+    return lefts, rights
 
 
 def _check_good_segment(N2, N, f, debug=False):
@@ -51,7 +185,7 @@ def _check_good_segment(N2, N, f, debug=False):
 @functools.lru_cache
 def latitude_Nf(N, f):
     N0 = 5.24e-3  # rad/s
-    f30 = 7.3e-5  # rad/s or dcpy.oceans.coriolis(30)
+    f30 = 7.3e-5  # rad/s or oceans.coriolis(30)
     return f / f30 * np.arccosh(N / f) / np.arccosh(N0 / f30)
 
 
@@ -68,7 +202,7 @@ def estimate_turb_segment(P, N2, lat, max_wavelength=256, debug=False, crit="mix
     N0 = 5.24e-3  # rad/s
     K_0 = 5e-6  # m²/s for a mixing efficiency of Γ=0.2
     ε_0 = 6.73e-10
-    f30 = 7.3e-5  # rad/s or dcpy.oceans.coriolis(30)
+    f30 = 7.3e-5  # rad/s or oceans.coriolis(30)
     π = np.pi
     f = np.abs(2 * (2 * π / 86400) * np.sin(lat * π / 180))
 
@@ -95,6 +229,9 @@ def estimate_turb_segment(P, N2, lat, max_wavelength=256, debug=False, crit="mix
     kzstar = (π * jstar / b) * (N / N0)
 
     isgood, flag = _check_good_segment(N2, N, f, debug=debug)
+
+    if not isgood:
+        return np.nan, np.nan, np.nan, np.nan, N ** 2, flag
 
     # TODO: check
     N2[~mask] = N2fit[~mask]
@@ -160,7 +297,7 @@ def estimate_turb_segment(P, N2, lat, max_wavelength=256, debug=False, crit="mix
 
     if debug:
         print(
-            f"ξgmvar: {ξgmvar:.3f}, ξvar: {ξvar:.3f}, N2: {N2fit.mean():.2e} K: {K:.2e}, ε: {ε:.2e}"
+            f"{P[0]:4.0f} — {P[-1]:4.0f}dbar: ξgmvar: {ξgmvar:.3f}, ξvar: {ξvar:.3f}, N2: {N2fit.mean():.2e} K: {K:.2e}, ε: {ε:.2e}"
         )
 
     return K, ε, ξvar, ξgmvar, N ** 2, flag
@@ -225,3 +362,190 @@ def do_mixsea_shearstrain(profile, depth_bins):
     ds = xr.concat([mixsea_to_xarray(resultpf), mixsea_to_xarray(resultal)], dim="kind")
     ds["kind"] = ["PF", "AL"]
     return ds
+
+
+def process_profile(profile, dz_segment=200, debug=True):
+    profile["σ_θ"] = eos.pden(
+        profile.cf["sea_water_salinity"],
+        profile.cf["sea_water_temperature"],
+        profile.cf["sea_water_pressure"],
+        0,
+    )
+    profile["γ"] = oceans.neutral_density(profile)
+
+    if debug:
+        profile_original = profile
+
+    profile = trim_mld_mode_water(profile)
+
+    S = profile.cf["sea_water_salinity"]
+    T = profile.cf["sea_water_temperature"]
+    P = profile.cf["sea_water_pressure"]
+
+    if profile.cf.sizes["Z"] < 13:
+        if debug:
+            raise ValueError("empty")
+        return ["empty!"]
+
+    lefts, rights = choose_bins(profile.cf["Z"].data, dz_segment)
+
+    results = {
+        var: np.full((len(lefts),), fill_value=np.nan)
+        for var in [
+            "Kρ",
+            "ε",
+            "ξvar",
+            "ξvargm",
+            "N2mean",
+            "γmean",
+            "flag",
+            "npts",
+            "pressure",
+            "Tzlin",
+            "Tzmean",
+            "mean_dTdz_seg",
+        ]
+    }
+    for var in ["γbnds", "pbnds"]:
+        results[var] = np.full((len(lefts), 2), fill_value=np.nan)
+
+    # N² calculation is the expensive step; do it only once
+    Zdim = profile.cf.axes["Z"][0]
+    N2full, _ = eos.bfrq(S, T, P, dim=Zdim, lat=profile.cf["latitude"])
+
+    # TODO: Need potential temperature so do this at the segment level
+    dTdzfull = (
+        -1 * profile.cf["sea_water_temperature"].cf.diff("Z") / P.cf.diff("Z")
+    ).cf.assign_coords({"Z": N2full.cf["Z"].data})
+    dTdzfull[Zdim].attrs["axis"] = "Z"
+
+    for idx, (l, r) in enumerate(zip(lefts, rights)):
+        seg = profile.cf.sel(Z=slice(l, r))
+
+        if seg.cf.sizes["Z"] == 1:
+            results["flag"][idx] = -1
+            continue
+
+        P = seg.cf["Z"]
+
+        # Argo in NATRE region: At 1000m sampling dz changes drastically;
+        # it helps to just drop that one point
+        seg = seg.where(P.cf.diff("Z") < 21, drop=True)
+
+        results["npts"][idx] = seg.cf.sizes["Z"]
+
+        # max dz of 20m; ensure min number of points
+        if results["npts"][idx] < np.ceil(dz_segment / 20):
+            results["flag"][idx] = -1
+            continue
+
+        # TODO: despike
+        # TODO: unrealistic values
+        N2 = N2full.cf.sel(Z=slice(P[0], P[-1]))
+
+        # TODO: Is this interpolation sensible?
+        dp = P.cf.diff("Z")
+        if dp.max() - dp.min() > 2:
+            dp = dp.median()
+            seg = seg.cf.interp(Z=np.arange(P[0], P[-1], dp.median()))
+            Pn2 = N2.cf["Z"]
+            N2 = N2.cf.interp(Z=np.arange(Pn2[0], Pn2[-1], dp.median()))
+
+        results["pressure"][idx] = (P.data[0] + P.data[-1]) / 2
+        results["pbnds"][idx, 0] = P.data[0]
+        results["pbnds"][idx, 1] = P.data[-1]
+
+        results["γmean"][idx] = seg.γ.mean()
+        results["γbnds"][idx, 0] = seg.γ.data[0]
+        results["γbnds"][idx, 1] = seg.γ.data[-1]
+
+        (
+            results["Kρ"][idx],
+            results["ε"][idx],
+            results["ξvar"][idx],
+            results["ξvargm"][idx],
+            results["N2mean"][idx],
+            results["flag"][idx],
+        ) = estimate_turb_segment(
+            N2.cf["Z"].data,
+            N2.data,
+            seg.cf["latitude"].data,
+            max_wavelength=dz_segment,
+            debug=debug,
+        )
+
+        results["Tzlin"][idx] = (
+            seg.cf["sea_water_temperature"]
+            .cf.polyfit("Z", deg=1)
+            .sel(degree=1)
+            .polyfit_coefficients.values
+            * -1
+        )
+        dTdz = dTdzfull.cf.sel(Z=slice(l, r))
+
+        results["mean_dTdz_seg"][idx] = dTdz.cf.mean("Z")
+
+        dTdz_fit = xr.polyval(
+            N2.cf["Z"], dTdz.cf.polyfit("Z", deg=2).polyfit_coefficients
+        )
+        results["Tzmean"][idx] = dTdz_fit.mean().data
+
+        # if debug:
+        #     import matplotlib.pyplot as plt
+
+        #     plt.figure()
+        #     dTdz.plot()
+        #     dTdz_fit.plot()
+
+    dataset = results_to_xarray(results, profile)
+
+    if debug:
+        plot_profile_turb(profile_original, dataset)
+
+    return dataset
+
+
+def plot_profile_turb(profile, result):
+    if all(result.ε.isnull().data):
+        print("no output!")
+        return
+    p_edges = cfxr.bounds_to_vertices(result.p_bounds, bounds_dim="nbnds")
+
+    f, axx = plt.subplots(1, 4, sharey=True)
+
+    ax = dict(zip(["T", "ξ", "strat", "turb"], axx.flat))
+    xlabels = ["$T$", "$ξ$ var", "", ""]
+
+    ax["S"] = ax["T"].twiny()
+    ax["γ"] = ax["T"].twiny()
+    plots.set_axes_color(ax["S"], "r")
+    plots.set_axes_color(ax["γ"], "teal")
+
+    profile.TEMP.cf.plot(ax=ax["T"], marker=".", markersize=4)
+    profile.PSAL.cf.plot(ax=ax["S"], color="r", _labels=False)
+    profile.γ.cf.plot(ax=ax["γ"], color="teal", _labels=False)
+
+    title = ax["T"].get_title()
+    [a.set_title("") for a in axx.flat]
+
+    result.ξvar.cf.plot(ax=ax["ξ"], _labels=False)
+    result.ξvargm.cf.plot(ax=ax["ξ"], _labels=False)
+    ax["ξ"].legend(["obs", "GM"])
+
+    (9.81 * 1.7e-4 * result.Tzmean).cf.plot(ax=ax["strat"], _labels=False)
+    result.N2mean.cf.plot(ax=ax["strat"], _labels=False)
+    ax["strat"].legend(["$gαT_z$", "$N^2$"])
+
+    result.ε.cf.plot(ax=ax["turb"], _labels=False)
+    result.χ.cf.plot(ax=ax["turb"], _labels=False, xscale="log")
+    ax["turb"].legend(["χ", "ε"])
+
+    plots.liney([result.Tmld, result.Tmode], color="k", ax=axx.flat)
+    plots.liney([result.σmld, result.σmode], color="b", ax=axx.flat)
+
+    for lab, a in zip(xlabels, axx.flat):
+        a.set_xlabel(lab)
+        a.set_yticks(p_edges, minor=True)
+        a.grid(True, axis="y", which="minor")
+
+    f.suptitle(title)
