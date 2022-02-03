fc19330e506dc9ca24d1937f7435eec5efd470bb
dcherian <deepak@cherian.net>
2022-02-03 14:18:41 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
 from setuptools import setup
 
-setup()
+setup(
+    # The package metadata is specified in setup.cfg but GitHub's downstream dependency graph
+    # does not work unless we put the name this here too.
+    name="dcpy",
+    use_scm_version={
+        "write_to": "dcpy/_version.py",
+        "write_to_template": '__version__ = "{version}"',
+        "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
+    },
+)
